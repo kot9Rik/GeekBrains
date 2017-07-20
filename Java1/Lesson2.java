b8d073904eb9c3921d31e7d8e1436e593ede924c
kot9Rik <vvedenskiyav@mail.ru>
2017-07-20 18:39:48 +0300

    Перемещено в папку Java1 Moved to Java1 folder

---
+++ Java1/Lesson2.java
@@ -0,0 +1,13 @@
+/**
+ * Java. Level 1. Lesson 2. Example of homework
+ *
+ * @author Aleksandr Vvedenskiy
+ * @version dated Jul 20, 2017
+ */
+ 
+ class Lesson2 {
+	 
+	 public static void main(String[] args){
+		 
+	 }
+ }
