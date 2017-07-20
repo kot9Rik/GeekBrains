4197dcc42efffe927d27c0c55d6af6019321953d
kot9Rik <vvedenskiyav@mail.ru>
2017-07-20 19:12:50 +0300

    (ДЗ Java1 Lesson2) cоздан класс Lesson2, сделано первое задание. (DZ Java1 Lesson2) Create a class Lesson2, made the first task.

--- Java1/Lesson2.java
+++ Java1/Lesson2.java
@@ -8,6 +8,25 @@
  class Lesson2 {
 	 
 	 public static void main(String[] args){
-		 
+		 invertArray();
 	 }
+	 
+	/**
+     * 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+     * 	  Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. 
+	 *    С помощью цикла и условия заменить 0 на 1, 1 на 0.
+     */
+	static void invertArray() {
+        int[] a = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+		String str = "";
+		for (int i = 0; i < a.length; i++) {
+		    if (a[i] == 0) a[i] = 1;
+			else a[i] = 0;
+		} 
+		for (int i = 0; i < a.length; i++) {
+		    str = str + " " + a[i];	
+		}
+		System.out.print(str);
+	}	
+	
  }
