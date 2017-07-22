60cee86e1591a0e587186994e76d752344be1a3a
kot9Rik <vvedenskiyav@mail.ru>
2017-07-22 13:49:04 +0300

    (ДЗ Java1 Lesson4) cоздан класс Lesson4, игра крестики-нолики в консоле. (DZ Java1 Lesson4) created the class Lesson4, the game tic-tac-toe in console.

---
+++ Java1/Lesson4.java
@@ -0,0 +1,114 @@
+/**
+ * Java. Level 1. Lesson 4. Example of homework 
+ *(Tic-tac-toe in console)
+ *
+ * @author Aleksandr Vvedenskiy
+ * @version dated Jul 22, 2017
+ */
+ 
+import java.util.*;
+
+class Lesson4 {
+
+    final int SIZE = 3; // size of the game map
+    final char DOT_X = 'x'; // sign of human
+    final char DOT_O = 'o'; // sign of AI
+    final char DOT_EMPTY = '.'; // sign of empty cell
+    char[][] map = new char[SIZE][SIZE];
+    Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
+
+    public static void main(String[] args) {
+        new Lesson4().go();
+    }
+
+    void go() {
+        initMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("AI WIN!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER.");
+    }
+
+    void initMap() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                map[i][j] = DOT_EMPTY;
+    }
+
+    void printMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++)
+                System.out.print(map[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Enter X and Y (1-3):");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_O;
+    }
+
+    boolean checkWin(char dot) {
+        // check horizontals and verticals
+        for (int i = 0; i < SIZE; i++)
+            if ((map[i][0] == dot && map[i][1] == dot && map[i][2] == dot) ||
+                (map[0][i] == dot && map[1][i] == dot && map[2][i] == dot))
+                return true;
+        // check diagonals
+        if ((map[0][0] == dot && map[1][1] == dot && map[2][2] == dot) ||
+            (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot))
+            return true;
+        return false;
+    }
+
+    boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                if (map[i][j] == DOT_EMPTY)
+                    return false;
+        return true;
+    }
+
+    boolean isCellValid(int x, int y) {
+        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE)
+            return false;
+        if (map[y][x] == DOT_EMPTY)
+            return true;
+        return false;
+    }
+}
